8794a3fee03e42ec7be41be08c7cd29277367b2f
unknown <mutungaeric839@gmail.com>
2023-04-23 07:41:37 +0300

    using @php and @endphp to include php code

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,9 +1,13 @@
 
 <h1>{{ $heading }}</h1>{{-- //blade used to echo value of the variable expression --}}
 
+@php
+    $test = 1;
+@endphp
+{{ $test }}
+
 @foreach($listings as $listing){{-- //for each for array listings --}}
  <h2>{{ $listing['title'] }}</h2>
  <p>{{ $listing['description'] }}</p>
  @endforeach
- {{-- @foreach is a Blade directive that instructs the compiler to start a foreach loop. Similarly, @endforeach is a directive that tells the compiler to end the foreach loop.
- --}}
+
