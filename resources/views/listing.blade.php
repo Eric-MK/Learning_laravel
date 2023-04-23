ca7f873d13341ed1a6718123b38f7f8cf6fae354
unknown <mutungaeric839@gmail.com>
2023-04-23 07:37:56 +0300

    using blade foreachfunction

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,8 +1,9 @@
 
-<h1>{{ $heading }}</h1>
+<h1>{{ $heading }}</h1>{{-- //blade used to echo value of the variable expression --}}
 
-<?php foreach($listings as $listing): ?>{{-- //for each for array listings --}}
- <h2><?php echo $listing['title']; ?></h2>
- <p><?php echo $listing['description']; ?></p>
-<?php endforeach; ?>
-<p>yesss</p>
+@foreach($listings as $listing){{-- //for each for array listings --}}
+ <h2>{{ $listing['title'] }}</h2>
+ <p>{{ $listing['description'] }}</p>
+ @endforeach
+ {{-- @foreach is a Blade directive that instructs the compiler to start a foreach loop. Similarly, @endforeach is a directive that tells the compiler to end the foreach loop.
+ --}}
