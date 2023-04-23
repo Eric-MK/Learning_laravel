f968932f0789760a5d99254c0dff7713a2125532
unknown <mutungaeric839@gmail.com>
2023-04-23 07:49:59 +0300

    using @if to check if array  is empty or not by using count()

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,11 +1,15 @@
 
 <h1>{{ $heading }}</h1>{{-- //blade used to echo value of the variable expression --}}
 
-@php
-    $test = 1;
-@endphp
-{{ $test }}
+@if(count($listings) == 0)
+   <p>no listings found</p>
 
+@endif
+{{-- @if directive checks if the count of the $listings array is equal to zero. If it is,
+the code block between @if and @endif is executed
+ If the condition in the @if statement is not met,
+ the code block within the @if and @endif directives will be skipped and not outputted.
+--}}
 @foreach($listings as $listing){{-- //for each for array listings --}}
  <h2>{{ $listing['title'] }}</h2>
  <p>{{ $listing['description'] }}</p>
