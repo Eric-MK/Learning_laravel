897cd5dc115dcf733ba7ca258ef6570cde42f0e5
unknown <mutungaeric839@gmail.com>
2023-04-23 07:59:44 +0300

    using @unless function

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,17 +1,16 @@
 
 <h1>{{ $heading }}</h1>{{-- //blade used to echo value of the variable expression --}}
 
-@if(count($listings) == 0)
-   <p>no listings found</p>
-
-@endif
-{{-- @if directive checks if the count of the $listings array is equal to zero. If it is,
-the code block between @if and @endif is executed
- If the condition in the @if statement is not met,
- the code block within the @if and @endif directives will be skipped and not outputted.
---}}
-@foreach($listings as $listing){{-- //for each for array listings --}}
+@unless(count($listings) == 0) {{-- if not equals to zero execute
+ unless is not zero execute--}}
+@foreach($listings as $listing)
  <h2>{{ $listing['title'] }}</h2>
  <p>{{ $listing['description'] }}</p>
  @endforeach
 
+ @else
+ <p>no listings found</p>
+
+@endunless
+{{-- the @unless directive is similar to the @if directive, but it checks for the opposite condition.
+ the @unless directive checks if the count of the $listings array is not equal to zero.    --}}
