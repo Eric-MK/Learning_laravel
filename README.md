4e2d67da29771f447397e61c478ff2fb8ce84bc6
unknown <mutungaeric839@gmail.com>
2023-04-24 18:07:35 +0300

    create model for Listing through php artisan

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 *php artisan migrate:refresh* => refreshes the database schema by rolling back all migrations and then re-running them
 
 *php artisan migrate:refresh --seed* =>  refreshes the database schema by rolling back all migrations and then re-running them. Additionally, it also runs the database seeders to populate the database with initial data.
+
+*php artisan make:model "model_name"* => creating model
+
--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,53 +1,11 @@
 <?php
-namespace App\Models;  /* indicates that the class belongs to the App\Models namespace. This means that the class can be referred to using its fully qualified
-namespace (\App\Models\ClassName)
-This is a convention used in Laravel to group related model classes together
-and avoid naming conflicts with other classes that may be defined in other namespaces. */
 
-class Listing {
+namespace App\Models;
 
-    public static function all()
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    {
-
-       return [
-        [
-            'id' => 1,
-            'title' => 'Listing One',
-            'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit
-            . Aliquam quidem animi, sed delectus eligendi
-            temporibus et, fuga error possimus saepe rerum nemo,
-            voluptates perspiciatis distinctio! Voluptas
-            porro accusantium harum. Dignissimos!'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Listing Two',
-            'description' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit
-            . Aliquam quidem animi, sed delectus eligendi
-            temporibus et, fuga error possimus saepe rerum nemo,
-            voluptates perspiciatis distinctio! Voluptas
-            porro accusantium harum. Dignissimos!'
-        ]
-        ];
-    }
-
-
-    public static function find($id)
-    {
-        $listings = self::all(); //self is often used to access static properties and methods of the current class
-
-        foreach($listings as $listing)
-        {
-            if ($listing['id'] == $id)  {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
-
-
-
-
-
-?>
